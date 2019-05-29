101287695f625f119feaccdacb16edbe339868f0
swarnanjali <50619461+swarnanjali@users.noreply.github.com>
2019-05-29 10:17:39 +0530

    Update numeric.py

--- numeric.py
+++ numeric.py
@@ -1,5 +1,7 @@
-string=input()
-if string.isnumeric():
-    print('yes')
+b=input()
+for i in b:
+    if i not in ['1','2','3','4','5','6','7','8','9','0',"."]:
+        print("No")
+        break
 else:
-    print('no')
+    print("Yes")
