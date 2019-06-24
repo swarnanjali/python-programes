fb8b4f632dfd09fc53464ca5c9c6f52e1bccaaff
swarnanjali <50619461+swarnanjali@users.noreply.github.com>
2019-06-24 22:24:59 +0530

    Update aisomorphic.py

--- aisomorphic.py
+++ aisomorphic.py
@@ -1,8 +1,8 @@
-shi,se=map(str,input().split())
-if(len(shi) != len(se)):
+shi,sea=map(str,input().split())
+if(len(shi) != len(sea)):
     print('no')
 xs=[shi.count(char1) for char1 in shi]
-ys=[se.count(char1) for char1 in se]
+ys=[sea.count(char1) for char1 in sea]
 if(xs==ys):
     print('yes')
 else:
