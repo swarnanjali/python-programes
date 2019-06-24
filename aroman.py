a4c7a17cc402f13f7f4f214b20ac7491779648fa
swarnanjali <50619461+swarnanjali@users.noreply.github.com>
2019-06-24 22:17:14 +0530

    Create aroman.py

---
+++ aroman.py
@@ -0,0 +1,5 @@
+shi=input()
+se=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if(shi==se[i]):
+    print(i)
