3692c1c079211a5e4112081eb454bd75eef335eb
swarnanjali <50619461+swarnanjali@users.noreply.github.com>
2019-06-26 20:34:29 +0530

    Update arule.py

--- arule.py
+++ arule.py
@@ -1,11 +1,11 @@
 n3=int(input())
 ss=list(map(int,input().split()))
-L=[]
+La5=[]
 d=0
 for i in range(0,n3-2):
     for j in range(i,n3,2):
         d=d+ss[j]
-    L.append(d)
+    La5.append(d)
     d=0
-L.sort()
-print(L[-1])
+La5.sort()
+print(La5[-1])
